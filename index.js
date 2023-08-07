da437a92671110da926a1ac81295c49e343f6cdc
sina <sinaprvnd.webdev@gmail.com>
2023-08-07 11:42:45 +0330

    find method

--- index.js
+++ index.js
@@ -1066,12 +1066,52 @@ index.js:563 (8) ['best prog lang?', 'c++', 'js', 'phyton', 'php', 2, "that's d
 // console.log(movementInfo);
 // //['Movment 1: Deposited 1000', 'Movment 2: Withdrew 200', 'Movment 3: Withdrew 340', 'Movment 4: Deposited 120']
 
-//! MAX VAL WITH REDUCE
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// //! MAX VAL WITH REDUCE
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const maxVal = movements.reduce((acc, cur) => {
-  if (acc > cur) return acc;
-  else return cur;
-}, movements[0]);
+// const maxVal = movements.reduce((acc, cur) => {
+//   if (acc > cur) return acc;
+//   else return cur;
+// }, movements[0]);
 
-console.log(maxVal);
+// console.log(maxVal);
+
+// //! FIND METHOD
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const firstWithdrawal = movements.find((mov) => mov < 0);
+// console.log(firstWithdrawal); //-400
+
+// const account1 = {
+//   owner: "Jonas Schmedtmann",
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
+
+// const account2 = {
+//   owner: "Jessica Davis",
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
+
+// const account3 = {
+//   owner: "Steven Thomas Williams",
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
+
+// const account4 = {
+//   owner: "Sarah Smith",
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
+
+// const accounts = [account1, account2, account3, account4];
+
+// console.log(accounts.find((acc) => acc.owner === "Jessica Davis"));
+// //{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222}
